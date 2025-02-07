6845101291a7421cfcaeb5b586939c24d233d59f
indurukarthika <indurukarthika8@gmail.com>
2025-02-07 18:38:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function showData(){
+    //console.log("show data on pressed");
+    //let a=5;
+    //console.log("data:"+a);
+    //for(i=0;i<=5;i++){
+        //console.log(i);
+        let name=document.getElementById("uname");
+        console.log("name--->:"+name.value);
+    }
